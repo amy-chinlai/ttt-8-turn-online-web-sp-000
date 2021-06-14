db9ce94823b8da644ac675a0ad011e74d183e588
Amy Chin-Lai <amy.chinlai@gmail.com>
2021-06-14 16:18:50 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,7 +13,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-	if board[index].between?(0, 8) && !position_taken?
+	if board[index].between?(1, 9) && !position_taken?
 		return true
 	end
 end
