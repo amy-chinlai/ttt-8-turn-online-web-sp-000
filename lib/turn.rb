6380c058d3a6ab41a3561a16fbb571175483fd65
Amy Chin-Lai <amy.chinlai@gmail.com>
2021-06-14 16:17:44 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,12 @@ def display_board(board)
 	puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def position_taken?(board, index)
+	if board[index] == "X" || board[index] == "O"
+		return true
+	end
+end
+
 def valid_move?(board, index)
 	if board[index].between?(0, 8) && !position_taken?
 		return true
