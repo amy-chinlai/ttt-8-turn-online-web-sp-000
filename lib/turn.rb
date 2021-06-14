c6869549963fdef84cd8be6c6798e72803ec7439
Amy Chin-Lai <amy.chinlai@gmail.com>
2021-06-14 16:46:20 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -28,8 +28,7 @@ end
 
 def turn(board)
 	puts "Please enter 1-9:"
-	input = gets.strip
-	input_to_index(input)
+	input = gets.strip.input_to_index
 	until valid_move?(board, index)
 		turn(board)
 	end
